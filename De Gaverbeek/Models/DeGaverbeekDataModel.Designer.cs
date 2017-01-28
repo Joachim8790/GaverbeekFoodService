5d7b412d21bcb6dcd2dfb90ceb4a789a90ecef6e
Joachim Verschelde <r0485881@student.vives.be>
2017-01-28 13:21:31 +0100

    database update+webconfig

--- De Gaverbeek/Models/DeGaverbeekDataModel.Designer.cs
+++ De Gaverbeek/Models/DeGaverbeekDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'P:\Project De Gaverbeek\De Gaverbeek\De Gaverbeek\Models\DeGaverbeekDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\joach\Source\Repos\GaverbeekFoodService\De Gaverbeek\Models\DeGaverbeekDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
